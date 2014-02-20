bcbadb164eb5c25bff563aa60005ba5ce6177602
C-A de Salaberry <cadesalaberry@yahoo.com>
2014-02-20 17:48:46 -0500

    Add: page 8/43

---
+++ lab3/lpm_or0_inst.vhd
@@ -0,0 +1,5 @@
+lpm_or0_inst : lpm_or0 PORT MAP (
+		data0	 => data0_sig,
+		data1	 => data1_sig,
+		result	 => result_sig
+	);
---
+++ lab3/lpm_or2_inst.vhd
@@ -0,0 +1,4 @@
+lpm_or2_inst : lpm_or2 PORT MAP (
+		data0x	 => data0x_sig,
+		result	 => result_sig
+	);
